376861ac20b56d4f4918291cc2c14ec810507fb5
MattiaDippolito <dippo.matty@gmail.com>
2021-04-22 23:03:02 +0200

    Create script.js

---
+++ eseTagSelectDinamico/js/script.js
@@ -0,0 +1,15 @@
+function mostra1(){
+    document.getElementById("p1").hidden = !document.getElementById("p1").hidden;
+}
+function mostra2(){
+    document.getElementById("p2").hidden = !document.getElementById("p2").hidden;
+}
+function mostra3(){
+    document.getElementById("p3").hidden = !document.getElementById("p3").hidden;
+}
+function mostra4(){
+    document.getElementById("p4").hidden = !document.getElementById("p4").hidden;
+}
+function mostra5(){
+    document.getElementById("p5").hidden = !document.getElementById("p5").hidden;
+}
